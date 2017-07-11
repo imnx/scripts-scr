1dc7b8f53795a5c3707044cb9fcdade6110141a0
iMaNeX <imnx.au@outlook.com>
2017-07-12 02:06:57 +1000

    updated imnx.sh.

--- imnx.sh
+++ imnx.sh
@@ -15,16 +15,20 @@ deb-src http://mirror.internode.on.net/pub/debian/ stretch-updates main contrib 
 apt-get update
 
 # Upgrade distro and install required packages.
-apt-get -y dist-upgrade
-apt-get -y install curl sudo perl-base gpm gfxboot gxfboot-themes plymouth plymouth-themes plymouth-theme-hamara open-vm-tools-dev python python-pip wget net-tools apt-utils psmisc dnsutils dialog pv build-essential cmake bison flex nasm yasm automake autoconf autogen autopoint autoproject autotools-dev libtool intltool doxygen texinfo texi2html byacc git cvs subversion mercurial openssh-server cifs-utils samba
+apt-get -qy dist-upgrade
+apt-get -qy install openssh-server cifs-utils samba curl sudo perl-base gpm gfxboot gxfboot-themes \
+plymouth plymouth-themes plymouth-theme-hamara open-vm-tools-dev python python-pip wget net-tools apt-utils \
+psmisc dnsutils dialog pv build-essential cmake bison flex nasm yasm automake autoconf autogen autopoint \
+autoproject autotools-dev libtool intltool doxygen texinfo texi2html byacc git cvs subversion mercurial \
+mc irssi libpurple-dev libjson-glib-dev libncurses5-dev libncursesw5-dev chromium gnome-desktop gdm3 xserver-xorg \
+xorg-core
 
 # Install GitLab-CE.
-apt-get install curl openssh-server ca-certificates postfix
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
 
-git clone -b master --depth=1 https://github.com/maximilienGilet/Elegant-GRUB2 /usr/src/elegant-grub2
-cd "/usr/src/elegant-grub2"
-./install.sh
+# git clone -b master --depth=1 https://github.com/maximilienGilet/Elegant-GRUB2 /usr/src/elegant-grub2
+# cd "/usr/src/elegant-grub2"
+# ./install.sh
 
 cd "${curDIR}"
 
@@ -32,10 +36,10 @@ echo "GRUB_DEFAULT='0'
 GRUB_TIMEOUT='5'
 GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
 GRUB_CMDLINE_LINUX_DEFAULT='net.ifnames=0 splash quiet'
-GRUB_CMDLINE_LINUX='net.ifnames=0 splash'
+GRUB_CMDLINE_LINUX='net.ifnames=0 splash quiet'
 
-GRUB_GFXMODE='1366x768x32'
-GRUB_GFXPAYLOAD='1366x768x32'
+GRUB_GFXMODE='1360x768'
+GRUB_GFXPAYLOAD='1360x768'
 GRUB_THEME=''
 
 GRUB_DISABLE_LINUX_UUID='false'
